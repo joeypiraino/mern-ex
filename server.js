a8b698b85c977e672f4282b10fbe944df50ba6ef
Big Sic <joeypiraino@gmail.com>
2020-09-02 20:18:55 -0500

    file updates

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -10,15 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/reack-reading-list"
+);
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function () {
+	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
